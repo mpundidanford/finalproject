6e23bfe0478f8d97651d9fa5d3044f62c15b1397
mpundidanford <danfordmpundi069@gmail.com>
2020-04-14 01:55:30 -0700

    progress

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
+import { urlencoded } from 'express';
+import session from 'express-session';
+
 var express = require('express');
-var express= express.router()
 var mongoose = require('mongoose');
+var fs = require('fs');
+var cookieSession = require('cookie-session');
+var bcyrpt = require('bcrypt');
+var path = require('path');
+var cookieParser =  require(cookie-parser);
+
+
+var express= express.router()
+
+app.use(express.urlencoded({extended :true}))
+app.use(cookieParser());
+
+//apply sesson 
+app.use(cookieSession({
+    name : session,
+    keys : [key1, key2],
+    maxAge : 3600*1000  
+    
+}))
+
+
+// create system user roles
+var Roles = {
+    loanOfficer : "loanOfficer",
+    lbloanOfficer : "lbloanOffier",
+    admin : "admin"
+}
+export{Roles}
+
+
+
+
+
+
+
 
 
 
