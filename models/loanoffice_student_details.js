62a985504e2cac100bd2f115cb8ff11a19916185
mpundidanford <danfordmpundi069@gmail.com>
2020-04-15 13:43:33 -0700

    progress

---
+++ models/loanoffice_student_details.js
@@ -0,0 +1,56 @@
+var express = require('express')
+var mongoose= require('mongoose');
+var schema = mongoose.schema()
+
+
+var studentSchema = mongoose.schema({
+
+    reg_no:{
+        type: String,
+        require : true,
+       },
+   form_four_number:{
+        type: String,
+        require : true,
+       },
+   firstName:{
+        type: Text,
+        require : true,
+       },
+   middleName:{
+        type : String,
+        require: true,
+        minlength : 5,
+        maxlength : 100,
+   },
+   lastName:{
+        type : String,
+        require: true,
+        minlength : 5,
+        maxlength : 100,
+           },
+  gender:{
+            type : String,
+            require: true,
+         },
+   year_of_study:{
+       type:Number,
+       require:true
+   },      
+    program_name:{
+        type: String,
+    }, 
+    account_no:{
+        type: integer,
+
+    },
+    bank_name:{
+        type : String
+    },    
+   
+   status:{
+       type:String,
+       require:true
+   }    
+})
+module.exports={student, studentSchema}
